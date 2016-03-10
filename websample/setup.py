303218d174f6f5438e7dcd2a85d81f7b04373eac
Soumya <soumya@zemosolabs.com>
2016-03-10 12:31:40 +0530

    Added a basic hello world python website

---
+++ websample/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Soumya',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'p.soumya1996@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['WEB'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
